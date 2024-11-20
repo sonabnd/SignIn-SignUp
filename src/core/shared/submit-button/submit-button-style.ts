11945bf283a5c251ca21c0f586c4ee67b8749628
sonabnd <sonabandaliyeva@gmail.com>
2024-11-20 11:08:02 +0400

    some changes

--- src/core/shared/submit-button/submit-button-style.ts
+++ src/core/shared/submit-button/submit-button-style.ts
@@ -1,11 +1,13 @@
 import { createUseStyles } from "react-jss";
+import sizes from "../../../assets/styles/abstracts/sizes";
+import { rem } from "../../../assets/styles/abstracts/functions";
 
 const styles = {
     button: {
-      width: "100%",
-      height: "50px",
-      fontSize: "16px",
-      fontWeight: "500",
+      width: '100%',
+      height: rem(50),
+      fontSize: sizes.pcLittleText,
+      fontWeight: sizes.mediumFontWeight,
       boxShadow: '0px 4px 19px 0px #7793414D',
     },
   };
